ea5515427b988d64907f432200c8c5acdd3c9b60
svnday <40030806+svnday@users.noreply.github.com>
2022-02-01 18:10:41 -0600

    creation of user routes

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init (
+    {
+        // define an id column
+        id: {
+            // use the special sequelize datatypes object to prove what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's 'not null' option
+            allowNull: false,
+            // instruct that this is the primary key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false we can run our data through validation before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // tghis means password must be at least 4 char long
+                len: [4]
+            }
+        }
+    },
+    {
+        // table config options go here
+
+        // pass in our imported sequelize connection (the direc connection to our db)
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // dont pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel casing
+        underscored: true,
+        // make it so our model name stays lowercase
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
